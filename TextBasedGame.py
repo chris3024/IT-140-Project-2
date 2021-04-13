714c7894d08cf6eb5093ec70fe316b119ef03b3a
Chris <chris3024@live.com>
2021-04-13 11:47:32 -0500

    adding comments to help understand game functions

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,5 +1,6 @@
 # Christopher Sharp
 
+# function to display playing instructions
 def show_instructions():
     print("\nWelcome to Moreside Castle!\n"
           "Collect 6 items to win the game, or meet your demise at the hands of the Giant Troll.\n"
@@ -8,6 +9,7 @@ def show_instructions():
     print("-----------------------------------")
 
 
+# function to display player status
 def player_status(inventory, current_room, rooms):
     print('\nInventory:', inventory)
     print('You are in', current_room)
@@ -16,9 +18,12 @@ def player_status(inventory, current_room, rooms):
     print("-----------------------------------")
 
 
+# main game function
 def main():
+    # inventory to hold items
     inventory = []
 
+    # dictionary to link rooms and items to rooms
     rooms = {
         'Grand Foyer': {'East': 'Great Hall'},
         'Great Hall': {'South': 'Common Room', 'North': 'Library', 'East': 'Classroom',
@@ -31,12 +36,16 @@ def main():
         'Dungeon': {'South': 'Classroom', 'item': 'Giant Troll'}
     }
 
+    # setting starting room
     current_room = 'Grand Foyer'
 
+    # calling instructions function
     show_instructions()
 
+    # game play loop
     while True:
 
+        # calling player status function
         player_status(inventory, current_room, rooms)
 
         # getting user command
@@ -48,6 +57,7 @@ def main():
             if command in rooms[current_room]:
                 current_room = rooms[current_room][command]
 
+                # checking for villain encounter and if all items are in inventory
                 if current_room == 'Dungeon':
                     print('You are now battling the Giant Troll')
                     if len(inventory) >= 6:
@@ -56,12 +66,16 @@ def main():
                         print('NOM..NOM..You were eaten by the troll')
                     break
 
+            # move input validation
             else:
                 print('Invalid Move. There is no room to the {}'.format(command))
 
+        # branching to pick up items
         elif command.split()[0] == 'get':
             command = command.split()[1].capitalize()
             if command in rooms[current_room]['item']:
+
+                # input validation for item spellings and adding to inventory
                 if len(command) == len(rooms[current_room]['item']):
                     inventory.append(command)
                     del rooms[current_room]['item']
@@ -69,9 +83,10 @@ def main():
                     print('Invalid input')
             else:
                 print('I don\'t see that here')
-
+        # overall input validation
         else:
             print('Invalid Command')
 
 
+# calling main function to run program
 main()
