94f1c18946dd9b9898222ace38e10d9d5d5e9af0
Chris <chris3024@live.com>
2021-04-13 11:26:06 -0500

    fixed loop break and added input validation for item

--- TextBasedGame.py
+++ TextBasedGame.py
@@ -1,10 +1,10 @@
 # Christopher Sharp
 
 def show_instructions():
-    print("\nWelcome to Moreside Castle!")
-    print("Collect 6 items to win the game, or meet your demise at the hands of the Giant Troll.")
-    print("Move commands: go South, go North, go East, go West")
-    print("Add to Inventory: get 'item name'")
+    print("\nWelcome to Moreside Castle!\n"
+          "Collect 6 items to win the game, or meet your demise at the hands of the Giant Troll.\n"
+          "Move commands: go South, go North, go East, go West\n"
+          "Add to Inventory: get 'item name'")
     print("-----------------------------------")
 
 
@@ -26,7 +26,7 @@ def main():
         'Common Room': {'North': 'Great Hall', 'East': 'Dormitory', 'item': 'Scarf'},
         'Dormitory': {'West': 'Common Room', 'item': 'Wand'},
         'Library': {'South': 'Great Hall', 'East': 'Observatory', 'item': 'Book'},
-        'Observatory': {'West': 'Library', 'item': 'Invisibility Cloak'},
+        'Observatory': {'West': 'Library', 'item': 'Cloak'},
         'Classroom': {'North': 'Dungeon', 'West': 'Great Hall', 'item': 'Potion'},
         'Dungeon': {'South': 'Classroom', 'item': 'Giant Troll'}
     }
@@ -42,27 +42,31 @@ def main():
         # getting user command
         command = input('Enter Command\n')
 
-        # if rooms[current_room]['item'] == 'Giant Troll':
-            #if len(inventory) == 6:
-                #print('Congrats! You defeated the Giant Troll')
-            #else:
-                #print('You were eaten by the Giant Troll! Game Over!')
-
-
         # game movements
         if command.split()[0] == 'go':
             command = command.split()[1].capitalize()
             if command in rooms[current_room]:
                 current_room = rooms[current_room][command]
 
+                if current_room == 'Dungeon':
+                    print('You are now battling the Giant Troll')
+                    if len(inventory) >= 6:
+                        print('Congrats! You defeated the Giant Troll')
+                    else:
+                        print('NOM..NOM..You were eaten by the troll')
+                    break
+
             else:
                 print('Invalid Move. There is no room to the {}'.format(command))
 
         elif command.split()[0] == 'get':
-            item = command.split()[1].capitalize()
-            if item in rooms[current_room]['item']:
-                del rooms[current_room]['item']
-                inventory.append(item)
+            command = command.split()[1].capitalize()
+            if command in rooms[current_room]['item']:
+                if len(command) == len(rooms[current_room]['item']):
+                    inventory.append(command)
+                    del rooms[current_room]['item']
+                else:
+                    print('Invalid input')
             else:
                 print('I don\'t see that here')
 
